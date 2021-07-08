966aa9c0eb545051020440cac64c211c542e57bf
Eric Chua <ericcyh130259@gmail.com>
2021-07-08 13:36:52 +0800

    1

---
+++ blog/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+class Blog(models.Model):
+    title=models.CharField(max_length=255)
+    pub_date=models.DateTimeField()
+    body=models.TextField()
+    image=models.ImageField(upload_to='images/')
+
+    def summary(self):
+        return self.body[:100]
+
+    def pubdatepretty(self):
+        return self.pub_date.strftime('%b %e %Y')
+
+    def __str__(self):
+        return self.title
