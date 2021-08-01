e52d1ebb14f032af88463a91a94bff5daa53fa0d
Eric <ericcyh130259@gmail.com>
2021-08-01 22:14:14 +0800

    added new page

--- blog/views.py
+++ blog/views.py
@@ -11,21 +11,23 @@ def detail(request, blog_id):
     return render(request, 'blog/detail.html', {'blog':detailblog})
 """
 
-def win10optimize(request):
-    return render(request, 'blog/win10optimize.html')
+def optimizewin10(request):
+    return render(request, 'home/optimizewin10.html')
 
 def step_1(request):
-        return render(request, 'blog/step_1.html')
+        return render(request, 'home/step_1.html')
 
 def step_2(request):
-        return render(request, 'blog/step_2.html')
-"""
-def step_3(request):
-        return render(request, 'blog/step_3.html')
+        return render(request, 'home/step_2.html')
 
+def step_3(request):
+        return render(request, 'home/step_3.html')
+"""
 def step_4(request):
         return render(request, 'blog/step_4.html')
 
 def step_5(request):
         return render(request, 'blog/step_5.html')
 """
+def step3_1(request):
+        return render(request, 'home/step_3/step3_1.html')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -9,10 +9,11 @@ urlpatterns = [
     #path('erichua/secure/secure/secure/admin', admin.site.urls),
     #path("",jobs.views.home,name='home'),
     #path("blog/", include('blog.urls')),
-    path("", blog.views.win10optimize, name='home'),
+    path("", blog.views.optimizewin10, name='home'),
     path("step_1", blog.views.step_1, name='step_1'),
     path("step_2", blog.views.step_2, name='step_2'),
-    #path("win10optimize/step_3", blog.views.step_3, name='step_3'),
+    path("step_3", blog.views.step_3, name='step_3'),
     #path("win10optimize/step_4", blog.views.step_4, name='step_4'),
     #path("win10optimize/step_5", blog.views.step_5, name='step_5'),
+    path("step3_1", blog.views.step3_1, name='step3_1'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
